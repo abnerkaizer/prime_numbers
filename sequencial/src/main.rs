32dcdea90b7678963347177b8e0fa3adb9a99a93
Abner Kaizer <abnerkaizer@protonmail.com>
2021-03-08 18:02:35 -0300

    Terminated

--- sequencial/src/main.rs
+++ sequencial/src/main.rs
@@ -1,32 +1,32 @@
 use std::time::Instant;
 
 fn main() {
-	let mut n:u128;
-	n = 0;
-	let now = Instant::now();
+    let mut n: u128;
+    n = 0;
+    let now = Instant::now();
     loop {
-    	if is_prime(n) {
-    		println!("{}", n);
-    	}
-    	
-    	let then = Instant::now();
-    	if then.duration_since(now).as_secs()>=1800 || n==u128::MAX {
-    		break;
-    	}
-    	n+=1;
+        if is_prime(n) {
+            println!("{}", n);
+        }
+
+        let then = Instant::now();
+        if then.duration_since(now).as_secs() >= 1800 {
+            break;
+        }
+        n += 1;
     }
     println!("THE END");
 }
 
-fn is_prime(n:u128) ->bool{
-	if n<=1 {
-		return false;
-	}
+fn is_prime(n: u128) -> bool {
+    if n <= 1 {
+        return false;
+    }
 
-	for a in 2..n {
-		if n%a == 0 {
-			return false;
-		}
-	}
-	true
+    for a in 2..n {
+        if n % a == 0 {
+            return false;
+        }
+    }
+    true
 }
