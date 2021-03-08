cadf209288423f49d874a20d5cedcb62e2625778
Abner Kaizer <abnerkaizer@protonmail.com>
2021-03-08 17:28:06 -0300

    Base

--- concurrent/src/main.rs
+++ concurrent/src/main.rs
@@ -1,3 +1,40 @@
+use std::time::Instant;
+use threadpool::ThreadPool;
+use num_cpus;
+
 fn main() {
-    println!("Hello, world!");
+	let mut n:u128;
+	n = 0;
+	//Criando um pool de threads com a quantidade de processadores logicos no PC.
+	//Nesse caso 4.
+	let pool = ThreadPool::new(num_cpus::get());
+	let now = Instant::now();
+    loop {
+
+    	pool.execute(move || task(n));
+    	
+    	let then = Instant::now();
+    	if then.duration_since(now).as_secs()>=1800 || n==u128::MAX {
+    		break;
+    	}
+    	n+=1;
+    }
+    println!("THE END");
+}
+fn task(n:u128){
+	if is_prime(n) {
+		println!("{}", n);
+	}
+}
+fn is_prime(n:u128) ->bool{
+	if n<=1 {
+		return false;
+	}
+
+	for a in 2..n {
+		if n%a == 0 {
+			return false;
+		}
+	}
+	true
 }
--- sequencial/src/main.rs
+++ sequencial/src/main.rs
@@ -1,3 +1,32 @@
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+	let mut n:u128;
+	n = 0;
+	let now = Instant::now();
+    loop {
+    	if is_prime(n) {
+    		println!("{}", n);
+    	}
+    	
+    	let then = Instant::now();
+    	if then.duration_since(now).as_secs()>=1800 || n==u128::MAX {
+    		break;
+    	}
+    	n+=1;
+    }
+    println!("THE END");
+}
+
+fn is_prime(n:u128) ->bool{
+	if n<=1 {
+		return false;
+	}
+
+	for a in 2..n {
+		if n%a == 0 {
+			return false;
+		}
+	}
+	true
 }
