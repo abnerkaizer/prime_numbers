4d19d4b7c762ce97f1554388753050dabcbae2ee
Abner Kaizer <abnerkaizer@protonmail.com>
2021-03-11 18:30:06 -0300

    Better is_prime function

--- concurrent/src/main.rs
+++ concurrent/src/main.rs
@@ -46,11 +46,12 @@ fn is_prime(n: usize) -> bool {
     if n <= 1 {
         return false;
     }
-
-    for a in 2..n {
-        if n % a == 0 {
+    let mut i = 2;
+    while i * i <= n {
+        if n % i == 0 {
             return false;
         }
+        i += 1;
     }
     true
 }
